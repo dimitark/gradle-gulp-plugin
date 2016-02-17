ee4cd50c3c9f9ca106700caf70f85e47fbe8f5a2
Dimitar Kotevski <dime.kotevski@gmail.com>
2016-02-17 15:20:02 +0100

    working dir quick fix

--- src/main/groovy/be/filipblondeel/gradle/gulp/task/GulpTask.groovy
+++ src/main/groovy/be/filipblondeel/gradle/gulp/task/GulpTask.groovy
@@ -8,13 +8,21 @@ class GulpTask extends NodeTask {
 
     private final static String GULP_SCRIPT = 'node_modules/gulp/bin/gulp.js'
 
+    def workingDir;
+
     public GulpTask() {
         this.group = GulpPlugin.GULP_GROUP_NAME
     }
 
     @Override
     void exec() {
-        def localGulp = this.project.file(GULP_SCRIPT)
+        def localGulp;
+        if (workingDir != null) {
+            localGulp = new File(workingDir, GULP_SCRIPT);
+        } else {
+            localGulp = this.project.file(GULP_SCRIPT)
+        }
+
         if (!localGulp.isFile()) {
             throw new GradleException(
                     "Gulp not installed in node_modules, please first run 'gradle ${GulpPlugin.GULP_INSTALL_NAME}'"
